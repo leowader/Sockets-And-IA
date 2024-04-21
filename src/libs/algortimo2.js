b808a532935f3e771e68ef8e237b2cd499e43f37
leowader <waderhe@gmail.com>
2024-04-21 17:26:04 -0500

    errores no lineales listos

--- src/libs/algortimo2.js
+++ src/libs/algortimo2.js
@@ -1,36 +1,56 @@
 const { data2 } = require("../data/data");
 const { Factivacion } = require("./Factivacion");
-const backPropagation = (data2, patron) => {
-  const { numeroCapas, w, u, fa } = data2;
+const {erroresNolineales} =require("./errorNol")
+const backPropagation = (data2, patron, yd) => {
+  const { numeroCapas, w, u, fa, numSalidas, numPatrones } = data2;
   let h = 0;
   let hi = 0;
   let H = [];
   let salidas = [];
+  let errL = [];
   let entradasCap = patron;
-  console.log("patron presentado ", entradasCap);
-  for (let c = 0; c < numeroCapas + 1; c++) {
-    if (H.length > 0) {
-      entradasCap = H;
-      H = [];
-    }
-    for (let i = 0; i < w[c][0].length; i++) {
-      h = 0; //reiniciar suma
-      for (let j = 0; j < w[c].length; j++) {
-        h += entradasCap[j] * w[c][j][i];
+  let ep = [];
+  for (let it = 0; it < 1; it++) { //numpatrones
+    let entradasCap = patron;
+    H = [];
+    salidas = [];
+    for (let c = 0; c < numeroCapas + 1; c++) {
+      if (H.length > 0) {
+        entradasCap[it] = H;
+        H = [];
       }
-      hi = h - u[c][i];
-      H.push(parseFloat(Factivacion(fa[c], hi).toFixed(5)));
+      for (let i = 0; i < w[c][0].length; i++) {
+        // console.log("ENTRADA PRESENTADA p: ",it,entradasCap[it]);
+        h = 0; //reiniciar suma
+        for (let j = 0; j < w[c].length; j++) {
+          h += entradasCap[it][j] * w[c][j][i];
+        }
+        hi = h - u[c][i];
+        H.push(parseFloat(Factivacion(fa[c], hi).toFixed(5)));
+      }
+      entradasCap[it] = H;
+      salidas.push({ h: H });
     }
-    entradasCap = H;
-    salidas.push({ h: H });
+    let yr = salidas[salidas.length - 1];
+    let sumError = 0;
+    console.log("yr =", yr);
+    console.log("yd: ", yd[it]);
+
+    for (let i = 0; i < yr.h.length; i++) {
+      errL.push(yd[it][i] - yr.h[i]); //calculamos el error lineal en el patron
+      sumError += Math.abs(yd[it][i] - yr.h[i]);
+    }
+    ep.push(+(sumError / numSalidas).toFixed(5));
+    console.log("error lineal :", errL);
+    console.log("ep: ", ep);
+    console.log("las H", salidas);
+    console.log("erro no lineal ", erroresNolineales(w, numeroCapas, errL));
   }
   return salidas;
 };
-const iteraciones = () => {
-  const { entradas } = data2;
 
-  for (let i = 0; i < entradas.length; i++) {
-    console.log("salida", backPropagation(data2, entradas[i]));
-  }
+const iteraciones = () => {
+  const { entradas, salidas } = data2;
+  backPropagation(data2, entradas, salidas);
 };
 module.exports = { iteraciones };
---
+++ src/libs/errorNol.js
@@ -0,0 +1,19 @@
+const erroresNolineales = (w, numeroCapas, errL) => {
+    let wReverse = w.reverse();
+    console.log("wreve", wReverse);
+    let errNo = [];
+    let SumErrNo = 0;
+    for (let c = 0; c < numeroCapas; c++) {
+      errNo.push([]);
+      for (let l = 0; l < w[c].length; l++) {
+        SumErrNo = 0;
+        for (let k = 0; k < w[c][0].length; k++) {
+          SumErrNo += errL[k] * w[c][l][k];
+        }
+        errNo[c][l] = +SumErrNo.toFixed(5);
+      }
+      errL = errNo[c];
+    }
+    return errNo;
+  };
+  module.exports={erroresNolineales}
