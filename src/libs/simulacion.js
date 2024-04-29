e4a4ece922dd77b9b4b23bce4431918a2d8d7cdd
leowader <waderhe@gmail.com>
2024-04-28 21:31:18 -0500

    ruta de simulacion

--- src/libs/simulacion.js
+++ src/libs/simulacion.js
@@ -1,3 +1,34 @@
-const simular =()=>{
-    
-}
+const { Factivacion } = require("../libs/Factivacion");
+const { getConfigurations } = require("../data/repositoryConfiguration");
+const simular = async (data) => {
+  const { entradas, numSalidas, numPatrones, salidas } = data;
+  let h = 0;
+  let H = [];
+  let salidasred = [];
+  let numeroCapas = 3;
+  const mipatron = entradas.slice();
+  const res = await getConfigurations();
+  let u = res[1].u;
+  let w = res[1].w;
+  let fa= res[1].fa
+  for (let p = 0; p < numPatrones; p++) {
+    H = [];
+    for (let c = 0; c < numeroCapas + 1; c++) {
+      if (H.length > 0) {
+        mipatron[p] = H;
+        H = [];
+      }
+      for (let i = 0; i < w[c][0].length; i++) {
+        h = 0; //reiniciar suma
+        for (let j = 0; j < w[c].length; j++) {
+          h += mipatron[p][j] * w[c][j][i];
+        }
+        H.push(parseFloat(Factivacion(fa[c], (h - u[c][i])).toFixed(1)));
+      }
+    }
+    salidasred.push(H);
+  }
+
+  return {data: salidasred}
+};
+module.exports = { simular };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,24 +4,26 @@ const multer = require("multer");
 const fs = require("fs");
 const { guardarTxt } = require("../libs/funciones");
 const upload = multer({ dest: "./src/uploads/" });
-routes.get("/ruta", (req, res) => {
-  res.send({ mensaje: "ruta ok" });
+const { simular } = require("../libs/simulacion");
+const { data2 } = require("../data/data");
+
+routes.get("/simular", async (_, res) => {
+  res.send(await simular(data2));
 });
+
 routes.post("/leer/wyu", upload.single("file"), async (req, res) => {
   const ruta = guardarTxt(req.file, fs);
   fs.readFile(ruta, "utf-8", (err, data) => {
     if (err) {
-      console.log("error: ", err);
+      res.send({ error: err.message });
     } else {
-      console.log("txt info",data);
+      res.send({ data: JSON.parse(data)});
     }
   });
-  res.send({ mensaje: req.file });
 });
+
 routes.get("/", (_req, res) => {
   res.send({ mensaje: "bienbenido al servidor de leowader" });
 });
-
-
 
 module.exports = routes;
