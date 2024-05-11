354e53b3e308ffb06fbf332ccc76c0c02c5ba70d
leowader <waderhe@gmail.com>
2024-05-11 18:54:37 -0500

    simulacion terminada por fin

--- src/libs/simulacion.js
+++ src/libs/simulacion.js
@@ -1,7 +1,7 @@
 const { Factivacion } = require("../libs/Factivacion");
-const {redondear}=require("./funciones")
+const { redondear } = require("./funciones");
 const simular = async (entradas, configuracion) => {
-  const { u, w, fa,numeroCapas } = configuracion;
+  const { u, w, fa, numeroCapas } = configuracion;
   let h = 0;
   let H = [];
   let salidasred = [];
@@ -18,12 +18,16 @@ const simular = async (entradas, configuracion) => {
         for (let j = 0; j < w[c].length; j++) {
           h += mipatron[p][j] * w[c][j][i];
         }
-        H.push(parseFloat(Factivacion(fa[c], (h - u[c][i])).toFixed(0)));
+        H.push(parseFloat(Factivacion(fa[c], h - u[c][i]).toFixed(0)));
       }
     }
     salidasred.push(H);
   }
-
-  return {data: salidasred}
+  for (let i = 0; i < salidasred.length; i++) {
+    if (salidasred[i] <= 0) {
+      salidasred[i] = [0];
+    }
+  }
+  return { data: salidasred };
 };
 module.exports = { simular };
